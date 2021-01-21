191f32f5a2dd30e5e307d79bc2a267515aedaf7d
alice270 <77018227+alice270@users.noreply.github.com>
2021-01-21 16:09:19 -0300

    oi

---
+++ TripleA/Assets/Scripts/DialogueScripts/DialogueManager2.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager2 : MonoBehaviour
+{
+
+
+    public static DialogueManager2 instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Fiz this" + gameObject.name);
+        }
+
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public GameObject dialogueBox;
+
+    public Text dialogueName;
+    public Text dialogueText;
+    public Image dialoguePortrait;
+    public float delay = 0.001f;
+    private string completeText;
+
+
+    public bool inDialogue;
+    public bool isCurrentlyTyping;
+    private bool buffer;
+
+    public Queue<DialogueBase.Info> dialogueInfo = new Queue<DialogueBase.Info>();
+
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void EnqueueDialogue(DialogueBase db)
+    {
+
+        buffer = true;
+        if (inDialogue) return;
+        inDialogue = true;
+        StartCoroutine(BufferTimer());
+
+        dialogueBox.SetActive(true);
+        dialogueInfo.Clear();
+
+        foreach (DialogueBase.Info info in db.dialogueInfo)
+        {
+            dialogueInfo.Enqueue(info);
+        }
+        DequeueDialogue();
+    }
+
+    public void DequeueDialogue()
+    {
+        if (isCurrentlyTyping == true)
+        {
+            if (buffer == true) return;  //ta aqui o problema falta i
+            CompleteText();
+            StopAllCoroutines();
+            isCurrentlyTyping = false;
+            return;
+        }
+
+
+        if (dialogueInfo.Count == 0)
+        {
+            EndofDialogue();
+            return;
+        }
+
+        DialogueBase.Info info = dialogueInfo.Dequeue();
+
+        dialogueName.text = info.myName;
+        dialogueText.text = info.myText;
+        dialoguePortrait.sprite = info.portrait;
+
+        dialogueText.text = "";
+        StartCoroutine(TypeText(info));
+    }
+
+
+    IEnumerator TypeText(DialogueBase.Info info)
+    {
+
+        isCurrentlyTyping = true;
+
+        foreach (char c in info.myText.ToCharArray())
+        {
+            yield return new WaitForSeconds(delay);
+            dialogueText.text += c;
+
+        }
+
+        isCurrentlyTyping = false;
+    }
+
+    IEnumerator BufferTimer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        buffer = false;
+    }
+
+    private void CompleteText()
+    {
+        dialogueText.text = completeText;
+    }
+
+    public void EndofDialogue()
+    {
+        dialogueBox.SetActive(false);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (inDialogue)
+            {
+                DequeueDialogue();
+            }
+        }
+    }
+}
---
+++ TripleA/Assets/Scripts/Utility/ativarscenetransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ativarscenetransition : MonoBehaviour
+{
+    public float interactRange = 2;
+    public GameObject MudarCena;
+    void Update()
+    {
+        if (Vector2.Distance(gameObject.transform.position, GameManager.instance.player.position) < interactRange)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                MudarCena.SetActive(true);
+            }
+        }
+    }
+}
